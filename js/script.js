cd648c17160ea353f07358373b646fd2cbdfde5d
aridpen <115594817+aridpen@users.noreply.github.com>
2022-12-02 16:02:25 -0600

    event listener added to letter blocks

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//name all JS variables
+//Dom selcectores
 
 let letterContainer = document.querySelector(".letter-container");
 let gallow = document.querySelector(".gallow-container");
@@ -8,36 +8,60 @@ let correct = document.querySelector(".rightLetter");
 let wrong = document.querySelector("wrongLetter");
 let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
-document.addEventListener("DOMContentLoaded", () => {
-  //create at least 5 word options (array)
-  const words = [
-    "taco",
-    "marshmallow",
-    "turducken",
-    "ramen",
-    "lasagna",
-    "chipotle",
-  ];
-  //random word choosen at random from the listed array of words
-  let randomWord = Math.floor(Math.random() * words.length);
-  console.log(randomWord);
+// document.addEventListener("DOMContentLoaded", () => {
+//create at least 5 word options (array)
+const words = [
+  "taco",
+  "marshmallow",
+  "turducken",
+  "ramen",
+  "lasagna",
+  "chipotle",
+];
 
-  //set up an empty array. for each word, make an empty space based off the length of randomWord each random word.
-  let answerUnderscore = [];
-  for (let i = 0; i < randomWord; i++) {
-    answerUnderscore[i] = "_";
-  }
-  console.log(answerUnderscore);
+//random word choosen at random from the listed array of words
+let randomWord = Math.floor(Math.random() * words.length);
+console.log(randomWord);
 
-  for (let i = 0; i < alphabet.length; i++) {
-    const letterBoxes = document.createElement("div");
-    letterBoxes.classList.add("letter");
-    letterBoxes.textContent = alphabet[i];
-    letterContainer.append(letterBoxes);
-  }
+//set up an empty array. for each word, make an empty space based off the length of randomWord each random word.
+let answerUnderscore = [];
+for (let i = 0; i < randomWord; i++) {
+  answerUnderscore[i] = "_";
+}
+console.log(answerUnderscore);
 
-  //   generateGrid();
+for (let i = 0; i < alphabet.length; i++) {
+  const letterBoxes = document.createElement("div");
+  letterBoxes.classList.add("letter");
+  letterBoxes.textContent = alphabet[i];
+  letterContainer.append(letterBoxes);
+}
+// alphabet.forEach(char => {
+//     const letterBoxes = document.createElement("div");
+//     letterBoxes.classList.add("letter");
+//     letterBoxes.textContent = char;
+//     letterContainer.append(letterBoxes);
+// })
+//letter[i]
+
+function hideUI() {
+  console.log("hideUI");
+}
+let letters = document.querySelectorAll(".letter");
+
+// console.log(letters);
+
+letters.forEach((letter) => {
+  letter.addEventListener("click", (e) => {
+    e.preventDefault();
+    //when block is clicked, letter is removed fromt the screen
+    //when correct letter is clicked, letter will populate on underscores
+    //
+    hideUI();
+  });
 });
+//   generateGrid();
+// });
 // create play area with underscores
 // start game
 // chose a random word each game
