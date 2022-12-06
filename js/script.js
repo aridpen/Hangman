3da7425d0f6126e23a2f24eed33101fbcf03390f
aridpen <115594817+aridpen@users.noreply.github.com>
2022-12-05 19:02:16 -0600

    added some images, switch statement for wrong guesses. overdue update

--- js/script.js
+++ js/script.js
@@ -2,9 +2,8 @@
 const letterContainer = document.querySelector(".letter-container");
 const textAnnouncer = document.querySelector(".text-announcer");
 const playAgain = document.querySelector(".play-again");
-const wrong = document.querySelector("wrong-letter");
 const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-
+let wrongGuessCount = 0;
 //create at least 5 word options (array)
 const words = [
   "taco",
@@ -14,10 +13,11 @@ const words = [
   // "lasagna",
   // "chipotle",
 ];
+let rightGuessCount = words.length;
 
 let randomWord = Math.floor(Math.random() * words.length); //random word choosen at random from the listed array of words
 let word = words[randomWord].split(""); //string method split turns word to an array of strings "T","A","C","O"
-
+console.log(words.length);
 //foreach letter (character) in word array create a div and append to guess-container
 //need span to allow letter textContent to work properly. otherwise whole div disapears on hidden.
 //created span element thst has a textContent of character. span is added to hidden classlist
@@ -44,58 +44,63 @@ function removeDOMElement(event) {
   event.target.remove();
 }
 
-//when block is clicked, letter is removed from the screen
 letters.forEach((letter) => {
+  //adding event listener to each letter
   letter.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    removeDOMElement(e);
-    //If I click leter A, Check if letter is in the array
-    //if letter is in array. Append to the div that has array letter
-    word.forEach((letter) => {
-      if (letter === e.target.textContent) {
-        const spans = document.querySelectorAll("span");
-        // console.log(e.path[0].innerText);
-        // wrongGuess();
-        //remove hidden class so that populated letter appears letter
-        spans.forEach((span) => {
-          if (span.textContent === e.target.textContent) {
-            span.classList.remove("hidden");
-            textAnnouncer.textContent = `Letter ${span.textContent} is Correct!! Choose Another Letter.`;
-          }
-          let currentLetter = e.path[0].innerText;
-          if (currentLetter !== span.textContent) {
-            console.log(`letter is b`);
-          }
-          // console.log(e.path[0].innerText);
-          checkWinner();
-        });
+    const spans = document.querySelectorAll("span");
+    let letter = e.target.textContent; //set letter to the text content of letter clicked(event.target)
+    let letterIndex = word.indexOf(letter); //set letterIndex to the index postion of clicked letter
+    if (letterIndex !== -1) {
+      //letters found in array will never have an index of -1 so by default wrong letters will be false
+      spans[letterIndex].classList.remove("hidden"); //remove correct letter from hidden class so that it shows on the line.
+      removeDOMElement(e);
+      textAnnouncer.textContent = `Letter ${letter} is Correct!! Choose Another Letter.`;
+    } else {
+      removeDOMElement(e);
+      textAnnouncer.textContent = `Oh NO! ${letter} is NOT correct!! Choose Another Letter.`;
+      wrongGuessCount++;
+      switch (wrongGuessCount) {
+        case 1:
+          document.getElementById("1").classList.remove("hidden");
+          break;
+        case 2:
+          document.getElementById("2").classList.remove("hidden");
+          break;
+        case 3:
+          document.getElementById("3").classList.remove("hidden");
+          break;
+        case 4:
+          document.getElementById("4").classList.remove("hidden");
+          break;
+        case 5:
+          document.getElementById("5").classList.remove("hidden");
+          break;
+        default:
+          textAnnouncer.textContent = `Too many guesses!!`;
       }
-      // function wrongGuess() {
-      //   if(e.path[0].innerText)
-      //   console.log(`wrongguess`);
-      // }
-    });
-    // e.path[0].innerText
-    // event.target.textAnnouncer.textContent = `nope`
+      if (wrongGuessCount > 5) {
+        gameOver();
+        // } else if (guessContainer !== "")
+        // console.log(`winner winner`);
+        // console.log(guessConatiner);
+
+        // if (wrongGuessCount > 5) {
+        //   console.log(`locos taco`);
+        // }
+      }
+    }
+    function gameOver() {
+      textAnnouncer.textContent = `GAME OVER!! Play Again`;
+    }
   });
 });
-function checkWinner() {}
 
-// e.target.classList.remove("hidden");
-
-// console.log(`${letter} is letter.`);
-// console.log(`${e.target.textContent} is target content.`);
-
-//only allow 6 incorrect guesses
-
-//if player selects the wrong letter 6 times game ends
-
-//reset game with play again// window.location.reload() refreshes page back to beginning state.
+//window reload is a no no :(
 playAgain.addEventListener("click", function () {
   window.location.reload();
 });
 
+//MVP GOALS
 //if array contains the textContent of the clicked letter, toggle hidden off
 //when correct letter is clicked, letter will populate on underscores//itterate over a word
 // create play area with underscores
